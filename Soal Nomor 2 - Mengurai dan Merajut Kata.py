26f2567b36748d56e5d9b94ed13077084e7068cd
fadh123 <80761778+fadh123@users.noreply.github.com>
2021-03-18 22:35:00 +0800

    Add files via upload

---
+++ Soal Nomor 2 - Mengurai dan Merajut Kata.py
@@ -0,0 +1,25 @@
+# Function Initialization :
+def urai(Code):
+
+def rajut(Code):
+
+ 
+ 
+# Use the function
+    print(urai('Code'))
+    print(urai('Python'))
+    print(urai('Purwadhika'))
+# Output:
+CCoCodCode
+PPyPytPythPythoPython
+PPuPurPurwPurwaPurwadPurwadhPurwadhiPurwadhikPurwadhika
+
+# Use the function
+    print(rajut('CCoCodCode'))
+    print(rajut('PPyPytPythPythoPython'))
+    print(rajut('PPuPurPurwPurwaPurwadPurwadhPurwadhiPurwadhikPurwadhika'))
+# Output:
+Code
+Python
+Purwadhika
+
